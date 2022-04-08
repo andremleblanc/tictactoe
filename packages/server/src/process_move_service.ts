f464d78b8a4e4c856dd58913206fccd6260e4371
André LeBlanc <andre@andreleblanc.me>
2022-04-07 22:57:58 -0400

    implement minimax algorithm for determining best score

--- packages/server/src/process_move_service.ts
+++ packages/server/src/process_move_service.ts
@@ -1,4 +1,4 @@
-import { Board } from "@mapistry/take-home-challenge-shared";
+import { Board, Marker } from "@mapistry/take-home-challenge-shared";
 import { WinningLines } from "./constants";
 
 export class ProcessMoveService {
@@ -8,19 +8,86 @@ export class ProcessMoveService {
         this.board = board;
     }
 
-    checkForWinner() {
-        const { board } = this
+    static calculateMinimaxScore(board: Board, depth: number, aiTurn: boolean) {
+        const scoreMap = {
+            O: 10,
+            X: -10,
+            tie: 0
+        }
+
+        let finalScore!: number;
+
+        const winner = this.checkForWinner(board)
+        if (winner !== null) return scoreMap[winner]
+
+        const isDraw = board.every(square => square !== null);
+        if (isDraw) return scoreMap.tie
+
+        for (let move = 0; move < board.length; move += 1) {
+            if (board[move] === null) {
+                const simulatedBoardState = board.slice()
+                let simulatedMoveScore: number
+                if (aiTurn) {
+                    simulatedBoardState[move] = Marker.o
+                    simulatedMoveScore = ProcessMoveService.calculateMinimaxScore(simulatedBoardState, depth + 1, false) - depth
+                    if (finalScore === undefined || simulatedMoveScore > finalScore) finalScore = simulatedMoveScore
+                } else {
+                    simulatedBoardState[move] = Marker.x
+                    simulatedMoveScore = ProcessMoveService.calculateMinimaxScore(simulatedBoardState, depth + 1, true) + depth
+                    if (finalScore === undefined || simulatedMoveScore < finalScore) finalScore = simulatedMoveScore
+                }
+            }
+        }
+
+        return finalScore
+    }
+
+    static calculateScoreForMove(board: Board, move: number) {
+        const simulatedBoardState = board.slice();
+        simulatedBoardState[move] = Marker.o
+        return ProcessMoveService.calculateMinimaxScore(simulatedBoardState, 0, false)
+    }
+
+    static checkForWinner(board: Board) {
         let winner = null;
 
         let winningLine = WinningLines.find(combination => combination.every(square => board[square] === 'X'));
-        if (winningLine === undefined) winningLine = WinningLines.find(e => e.every(square => board[square] === 'O'));
+        if (winningLine === undefined) winningLine = WinningLines.find(combination => combination.every(square => board[square] === 'O'));
         if (winningLine !== undefined) winner = board[winningLine[0]]
 
         return winner
     }
 
+    determineBestMove() {
+        const { board } = this
+        let bestScore;
+        let bestMove;
+        let score;
+
+        for (let move = 0; move < board.length; move += 1) {
+            if (board[move] === null) {
+                score = ProcessMoveService.calculateScoreForMove(board, move)
+
+                if (bestScore === undefined || score > bestScore) {
+                    bestScore = score
+                    bestMove = move
+                }
+            }
+        }
+
+        return bestMove
+    }
+
     call() {
-        const winner = this.checkForWinner()
-        return { board: this.board, winner }
+        const { board } = this
+        let winner = ProcessMoveService.checkForWinner(board)
+
+        if (winner) return { board, winner }
+
+        const move = this.determineBestMove()
+        if (move !== undefined) { board[move] = Marker.o }
+
+        winner = ProcessMoveService.checkForWinner(board)
+        return { board, winner }
     }
 }
